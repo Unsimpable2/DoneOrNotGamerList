d0b9b81cfd549f4dfd209f47076b01cb57a4b816
Witek <egertmasal@gmail.com>
2024-11-23 21:24:56 +0100

    Załadowanie tabel do bazy danych

---
+++ Server/Migrations/20241123201101_init.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GameName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    GameGenre = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    TotalTime = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserLogin = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    UserPassword = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    AccountDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserScoreData",
+                columns: table => new
+                {
+                    ScoreId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    GameId = table.Column<int>(type: "integer", nullable: false),
+                    GameStatus = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    TimePlayed = table.Column<float>(type: "real", nullable: true),
+                    GameplayScore = table.Column<float>(type: "real", nullable: true),
+                    StoryScore = table.Column<float>(type: "real", nullable: true),
+                    GraphicScore = table.Column<float>(type: "real", nullable: true),
+                    AudioScore = table.Column<float>(type: "real", nullable: true),
+                    QualityScore = table.Column<float>(type: "real", nullable: true),
+                    SumScore = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserScoreData", x => x.ScoreId);
+                    table.ForeignKey(
+                        name: "FK_UserScoreData_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserScoreData_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserScoreData_GameId",
+                table: "UserScoreData",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserScoreData_UserId",
+                table: "UserScoreData",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserScoreData");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
